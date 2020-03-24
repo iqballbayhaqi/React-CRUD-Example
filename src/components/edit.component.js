7d2b122db8f9b1050782386de18cf3f40ff04296
iqballbayhaqi <baihaqiiqbal323@gmail.com>
2020-03-24 12:07:04 +0700

    Add Component

---
+++ src/components/edit.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
+
+export default Edit extends Component {
+    render() {
+        return(
+            <div>
+                <p>Welcome to Edit Component !!</p>
+            </div>
+        )
+    }
+}
