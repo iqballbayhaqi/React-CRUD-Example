52b074e7a58228a84b443ec446b4d70ed0d111ce
iqballbayhaqi <baihaqiiqbal323@gmail.com>
2020-03-24 11:54:28 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return(
+      <div className="container">
+        <h2>React CRUD Tutorial</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
