48f466a4242005ab2c815730d6d241e5b963ab33
iqballbayhaqi <baihaqiiqbal323@gmail.com>
2020-03-24 16:06:12 +0700

    create the bootstrap form

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -3,8 +3,25 @@ import React, { Component } from 'react';
 export default class Create extends Component {
     render() {
         return(
-            <div>
-                <p>Welcome to Create Component !!s</p>
+            <div style={{marginTop: 10}}>
+                <h3>Add New Member</h3>
+                <form>
+                    <div className="form-group">
+                        <label>Add Person Name: </label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Add Job Title Name: </label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Add Phone Number: </label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Register Member" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         )
     }
