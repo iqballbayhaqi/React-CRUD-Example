65fa7768b7d008294d06d087fde779af6a63845a
iqballbayhaqi <baihaqiiqbal323@gmail.com>
2020-03-24 15:54:51 +0700

    configure React routing

--- src/components/edit.component.js
+++ src/components/edit.component.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from '@testing-library/react';
 
-export default Edit extends Component {
+export default class Edit extends Component {
     render() {
         return(
             <div>
--- src/components/index.component.js
+++ src/components/index.component.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react';
 
-export default class Index extends Compoent {
+export default class Index extends React.Component {
     render() {
         return (
             <div>
