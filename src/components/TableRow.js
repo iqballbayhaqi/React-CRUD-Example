a338aa46455fcbd8eaca9ba23868fa9e4ee9b3da
iqballbayhaqi <baihaqiiqbal323@gmail.com>
2020-03-26 15:08:03 +0700

    Delete the data function

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class TableRow extends Component {
+    constructor(props) {
+        super(props);
+        this.delete = this.delete.bind(this);
+    }
+    delete() {
+        axios.get('http://localhost:4000/member/delete/'+this.props.obj._id)
+            .then(console.log('Deleted'))
+            .catch(err => console.log(err))
+    }
+
     render() {
         return(
             <tr>
@@ -18,7 +29,7 @@ class TableRow extends Component {
                 <Link to={"/edit/"+this.props.obj._id} className="btn btn-primary">Edit</Link>
                 </td>
                 <td>
-                    <button className="btn btn-danger">Delete</button>
+                    <button onClick={this.delete} className="btn btn-danger">Delete</button>
                 </td>
             </tr>
         );
