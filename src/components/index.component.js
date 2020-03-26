d2b997ac0aa31c551a6b3c08adb522c18fb2b54f
iqballbayhaqi <baihaqiiqbal323@gmail.com>
2020-03-26 13:29:30 +0700

    display the backend data

--- src/components/index.component.js
+++ src/components/index.component.js
@@ -1,10 +1,45 @@
 import React, { Component} from 'react';
+import axios from 'axios';
+import TableRow from './TableRow';
 
 export default class Index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {member: []};
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:4000/member')
+        .then(response => {
+            this.setState({ member: response.data });
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    }
+    TabRow(){
+        return this.state.member.map(function(object, i){
+            return <TableRow obj={object} key={i} />
+        });
+    }
+
     render() {
         return (
             <div>
-                <p>Welcome to Index Component !!!</p>
+                <h3 align="center">Member List</h3>
+                <table className="table table-striped" style={{ marginTop: 20}}>
+                    <thead>
+                        <tr>
+                            <th>Person</th>
+                            <th>Job Title</th>
+                            <th>Phone Number</th>
+                            <th colSpan="2">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.TabRow() }
+                    </tbody>
+                </table>
             </div>
         )
     }
